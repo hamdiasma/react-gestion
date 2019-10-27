bfe0e76afdede1f1ea6ea21d42ff01ec3cfdb199
Malek Boubakri <malekbouba@gmail.com>
2019-10-27 12:32:15 +0100

    cleanups

--- src/components/pages/HomeContainer.jsx
+++ src/components/pages/HomeContainer.jsx
@@ -1,70 +1,7 @@
 import React from "react";
 
-class HomeContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      movies: null,
-      moviesNb: 0,
-      pagesNb: 0,
-      pageLength: 4,
-      currentPage: 1
-    };
-  }
-
-  async componentDidMount() {
-    const response = await fetch(
-      `http://localhost:5000/films/${this.state.currentPage}`
-    );
-    const content = await response.json();
-    this.setState({
-      movies: content.docs,
-      moviesNb: content.total,
-      pagesNb: content.pages
-    });
-  }
-
-  next = () => {
-    if (this.state.currentPage < this.state.pagesNb)
-      this.setState({ currentPage: this.state.currentPage + 1 }, () =>
-        this.refresh()
-      );
-  };
-
-  prev = () => {
-    if (this.state.currentPage > 1)
-      this.setState({ currentPage: this.state.currentPage - 1 }, () =>
-        this.refresh()
-      );
-  };
-
-  goTo = page => {
-    this.setState({ currentPage: page }, () => this.refresh());
-  };
-
-  refresh = async () => {
-    const response = await fetch(
-      `http://localhost:5000/films/${this.state.currentPage}`
-    );
-    const content = await response.json();
-    this.setState({
-      movies: content.docs,
-      moviesNb: content.total,
-      pagesNb: content.pages
-    });
-  };
-
-  toggleAdd = () => {
-    this.setState({ isAddOpen: !this.state.isAddOpen });
-  };
-
-  render() {
-    return (
-      <div className="container">
-        Home
-      </div>
-    );
-  }
-}
+const HomeContainer = () => {
+  return <div className="container">Home</div>;
+};
 
 export default HomeContainer;
