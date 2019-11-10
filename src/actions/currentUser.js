7facda08f0286795ee971cc6fde49cd53504daac
Malek Boubakri <malekbouba@gmail.com>
2019-11-10 13:41:38 +0100

    persist authentication in localstorage

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,10 +1,12 @@
 export const setUser = value => {
+  localStorage.setItem("currentUser", JSON.stringify(value));
   return {
     type: "SET_USER",
     payload: value
   };
 };
 export const unsetUser = () => {
+  localStorage.removeItem("currentUser");
   return {
     type: "UNSET_USER"
   };
--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,4 +1,8 @@
-const currentUser = (state = {}, action) => {
+const initialState = localStorage.getItem("currentUser")
+  ? JSON.parse(localStorage.getItem("currentUser"))
+  : {};
+
+const currentUser = (state = initialState, action) => {
   switch (action.type) {
     case "SET_USER":
       return action.payload;
